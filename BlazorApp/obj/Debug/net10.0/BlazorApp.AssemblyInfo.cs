c788a8ae14f9a5d82b564d4ffef9373219de1cb9
Ze_Je <kovalchuk.i_ipz23@rcit.ukr.education>
2026-03-12 00:11:44 +0200

    SQLite - створена база даних

--- BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f289bed44dcf01ed17a6e53da7d77ee1183c33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bffc6c23e804c722892facd78f291fe40f21c90")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
